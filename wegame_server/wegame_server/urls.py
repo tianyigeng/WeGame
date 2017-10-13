c6c2434181bf6c10bfed400e49ce88423a1d18c3
Tarnum Geng <i@tianyi.world>
2017-10-13 23:13:05 +0800

    add main_app

--- wegame_server/wegame_server/urls.py
+++ wegame_server/wegame_server/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from main_app.views import home
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+  url(r'^$', home, name='home'),
 ]
