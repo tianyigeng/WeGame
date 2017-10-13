6bd011021226a8bbd8d60e098f3733a319cf0708
Tarnum Geng <i@tianyi.world>
2017-10-13 23:18:17 +0800

    update wsgi

--- wegame_server/wegame_server/wsgi.py
+++ wegame_server/wegame_server/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = '/usr/local/WeGame/wegame_server'
+if path not in sys.path:
+  sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wegame_server.settings")
 
