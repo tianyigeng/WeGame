ea44088d0d6ad9738d401c51ccf1c85eb0eebf86
DianaZhang <>
2017-12-06 20:21:24 -0600

    bg

--- wegameFront/wegame/src/users/userRecommendation.js
+++ wegameFront/wegame/src/users/userRecommendation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import {IndexRoute, browserHistory} from 'react-router';
-import '../index.css';
+import './recommendation.css';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 /**
  * Created by dianazhang on 2017/10/20.
@@ -19,8 +19,17 @@ class userRecommendation extends React.Component {
 
                 <h1>Recommended Games</h1>
                 <p>Our recommendations are based on your game history.</p>
+
+                <div className="hotGame">
+                    <h3>Hottest Game</h3>
+
+                    <div className="pic">   </div>
+                </div>
+
             </div>
         );
     }
 }
 export default userRecommendation
+
+//<img src={'../../../../wordcloud/out.png'} alt="Hottest Game" className="WordCloud"/>
